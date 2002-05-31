0158a8b0544d36e0fa76d753c714c30ecb80e575
Ben Goodwin <cinergi@users.sourceforge.net>
2002-05-31 04:46:23 +0000

    Moved syslog.h here Removed unused FSIZ macro Removed unused D_ERROR; renumbered D_* defines Removed unnecessary CLOSE_ALL.  CLOSE_LINK must CLOSE_RESULT anyway Removed unnecessary \n's on debug lines Added #defines for some configuration defaults Lots of new comments Added boolean and return types so we check/set returns via enumurated     constants instead of 0's and 1's ... Added debug_flags to conf.global Rearranged some stuff for better readability

--- nss_mysql.h
+++ nss_mysql.h
@@ -21,6 +21,7 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+
 #ifdef HAVE_NSS_H
 #include <nss.h>
 #elif defined HAVE_NSS_COMMON_H
@@ -28,13 +29,19 @@
 #else
 #error I need either nss.h or nss_common.h!
 #endif
+
 #ifdef HAVE_MYSQL_MYSQL_H
 #include <mysql/mysql.h>
 #elif defined HAVE_MYSQL_H
 #include <mysql.h>
 #endif
+
 #include <sys/socket.h>
 #include <errno.h>
+
+#ifdef HAVE_SYSLOG_H
+#include <syslog.h>
+#endif
 
 #ifdef HAVE___FUNC__
 #define FNAME __func__
@@ -65,36 +72,58 @@ typedef enum
 #define PADSIZE         64              /* malloc this much more for queries
                                            to allow for format expansion */
 
-#define FOFS(x,y) ((int)&(((x *)0)->y)) /* Get field offset */
-#define FSIZ(x,y) (sizeof(((x *)0)->y)) /* Get field size */
+/* Use these as defaults until they're overriden via the config file */
+#define DEF_FACIL       LOG_AUTH
+#define DEF_PRIO        LOG_ALERT
+#define DEF_RETRY       30
+#define DEF_DFLAGS      D_ALL
 
-#define D_ERROR         0x0001
-#define D_MEMORY        0x0002
-#define D_FUNCTION      0x0004
-#define D_CONNECT       0x0008
-#define D_QUERY         0x0010
-#define D_PARSE         0x0020
-#define D_FILE          0x0040
+/* Debug flags; used in conf.global.debug_flags and the debug function */
+#define D_MEMORY        0x0001
+#define D_FUNCTION      0x0002
+#define D_CONNECT       0x0004
+#define D_QUERY         0x0008
+#define D_PARSE         0x0010
+#define D_FILE          0x0020
+#define D_ALL           0x0040 - 1
 
-#define CLOSE_RESULT    0x0001
-#define CLOSE_LINK      0x0002
-#define CLOSE_ALL       (CLOSE_RESULT | CLOSE_LINK)
+/* Flags to pass to the close_sql function */
+#define CLOSE_RESULT    0x0001      /* Nuke the result but leave link open */
+#define CLOSE_LINK      0x0002      /* Nuke result AND link */
 
-#define PTRSIZE sizeof (void *)
+#define PTRSIZE sizeof (void *)     /* My attempt at being portable */
 
-#define function_enter _nss_mysql_debug (FNAME, D_FUNCTION, "BEGIN\n");
-#define function_leave _nss_mysql_debug (FNAME, D_FUNCTION, "LEAVE: -\n");
+#define FOFS(x,y) ((int)&(((x *)0)->y)) /* Get Field OfFSet */
+/* These should be used in almost every function for debugging purposes */
+#define function_enter _nss_mysql_debug (FNAME, D_FUNCTION, "BEGIN");
+#define function_leave _nss_mysql_debug (FNAME, D_FUNCTION, "LEAVE: -");
 #define function_return(to_return)                                            \
   {                                                                           \
     if (to_return == NSS_TRYAGAIN)                                            \
       errno = ERANGE;                                                         \
-    _nss_mysql_debug (FNAME, D_FUNCTION, "LEAVE: %d\n",  to_return);          \
+    _nss_mysql_debug (FNAME, D_FUNCTION, "LEAVE: %d",  to_return);            \
     return to_return;                                                         \
   }
 
 typedef unsigned char _nss_mysql_byte;        /* for pointer arithmetic */
 
-/* Parse types */
+/* To the untrained eye, this looks like my version of a boolean.  It's
+   really my secret code for taking over the universe ... */
+typedef enum {
+    nfalse,
+    ntrue
+} nboolean;
+
+/* It's SO damn confusing when functions use a return of 0 for success and
+   1 for failure, especially amidst functions that have a boolean return
+   type.. so use this instead.  PLEASE.  I BEG YOU. */
+typedef enum {
+    RETURN_SUCCESS,
+    RETURN_FAILURE
+} freturn_t;
+
+/* Parse types for the 'lis' functions.  This is how I accomplish
+   loading data into a struct without referencing the structure's members */
 typedef enum {
     FT_NONE,
     FT_INT,
@@ -103,13 +132,15 @@ typedef enum {
     FT_PPCHAR,   /* char ** Pointer to pointer to char (unallocated) */
     FT_UINT,
     FT_ULONG,
-    FT_SYSLOG,   /* incoming string, convert to an integer */
+    FT_SYSLOG,   /* incoming string, convert to appropriate integer */
 } ftype_t;
 
+/* Mostly used to use a string to describe where in a structure something
+   goes.  I overload it's purpose in a couple places though */
 typedef struct {
     char    *name;
     int     ofs;
-    ftype_t type;
+    int     type;
 } field_info_t;
 
 /* Sql queries to execute for ... */
@@ -139,46 +170,57 @@ typedef struct {
 } sql_conf_t;
 
 typedef struct {
-    int             retry;
-    int             syslog_facility;
-    int             syslog_priority;
+    int             retry;              /* retry server #0 every x seconds */
+    int             syslog_facility;    /* facility to log with */
+    int             syslog_priority;    /* Log at this prio and higher */
+    int             debug_flags;        /* if prio = debug, log only these */
 } global_conf_t;
 
 typedef struct {
-    int             valid;
-    int             num_servers;
-    global_conf_t   global;
-    sql_conf_t      defaults;
-    sql_conf_t      sql[MAX_SERVERS];
+    nboolean        valid;              /* Have we loaded config yet? */
+    int             num_servers;        /* 1 .. MAX_SERVERS */
+    global_conf_t   global;             /* settings that apply everywhere */
+    sql_conf_t      defaults;           /* [defaults] config section */
+    sql_conf_t      sql[MAX_SERVERS];   /* [server] sections */
 } conf_t;
 
+/* As soon as a MySQL link is established, save the results of
+   getsockname and getpeername here so we can make sure our
+   socket hasn't been mutilated by an outside program. */
 typedef struct {
-    struct sockaddr local;
-    struct sockaddr remote;
+    struct sockaddr local;              /* getsockname */
+    struct sockaddr remote;             /* getpeername */
 } socket_info_t;
 
-/* Used for static-variable state retention */
+/* All information regarding existing MySQL link */
 typedef struct {
-    MYSQL_RES       *mysql_result;  /* Current result set */
-    int             server_num;     /* Server # currently connected to */
-    int             idx;            /* result set index (get/set/end*ent) */
-    time_t          last_attempt;   /* last time we tried primary server */
-    int             valid_link;     /* is LINK below valid? */
-    MYSQL           link;           /* Current MySQL link info */
-    socket_info_t   sock_info;      /* store get[sock|peer]name results here */
-    char            *query[MAX_SERVERS];
-} state_t;
+    nboolean        valid;          /* Are we connected to a server? */
+    time_t          last_attempt;   /* Last time we tried server #0 */
+    int             server_num;     /* 0 .. MAX_SERVERS - 1 */
+    int             idx;            /* 0 .. # - For *ent routines */
+    MYSQL_RES       *result;
+    MYSQL           link;
+    socket_info_t   sock_info;      /* See above */
+} con_info_t;
 
+/* nss_mysql.c */
 void _nss_mysql_debug(char *function, int flags, char *fmt, ...);
-NSS_STATUS _nss_mysql_close_sql (state_t *state, int flags);
-NSS_STATUS _nss_mysql_init(conf_t *conf, state_t *state);
-NSS_STATUS _nss_mysql_run_query(conf_t conf, state_t *state);
-NSS_STATUS _nss_mysql_load_result(void *result, char *buffer,size_t buflen,
-                                  state_t *state, field_info_t *fields,
-                                  int idx);
-void _nss_mysql_log_error (char *function, char *fmt, ...);
+void _nss_mysql_log (int priority, char *fmt, ...);
+
+/* nss_support.c */
+NSS_STATUS _nss_mysql_close_sql (int flags);
+NSS_STATUS _nss_mysql_init(conf_t *conf);
+NSS_STATUS _nss_mysql_run_query(conf_t conf, char **query_list);
+NSS_STATUS _nss_mysql_load_result(void *result, char *buffer, size_t buflen,
+                                  field_info_t *fields);
+void _nss_mysql_reset_ent (void);
+nboolean _nss_mysql_active_result (void);
+void _nss_mysql_inc_ent (void);
+
+/* nss_config.c */
 NSS_STATUS _nss_mysql_load_config (conf_t *conf);
 
+/* nss_structures.c */
 extern field_info_t passwd_fields[];
 extern field_info_t spwd_fields[];
 extern field_info_t group_fields[];
