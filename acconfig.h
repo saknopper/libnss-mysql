307f9fc42a745d3214e602a6366d7a49c9ce4393
Ben Goodwin <cinergi@users.sourceforge.net>
2002-05-31 04:38:11 +0000

    Debugging goes to syslog now.  No need for DEBUG_FILE

--- acconfig.h
+++ acconfig.h
@@ -1,9 +1,6 @@
 /* define to int if your system doesn't define it for you */
 #undef intptr_t
 
-/* Define this with a file to send debugging information to */
-#undef DEBUG_FILE
-
 /* */
 #undef MAINCFG
 
