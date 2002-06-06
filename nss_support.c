c5fc65df9f3cca836814b06c2ca90d48be8a39c4
Ben Goodwin <cinergi@users.sourceforge.net>
2002-06-06 19:08:09 +0000

    Fixed bug with saving/checking socket integrity Better method of saving to BUFFER Use generic memory alloc routines (which do their own logging)

--- nss_support.c
+++ nss_support.c
@@ -34,38 +34,6 @@ static const char rcsid[] =
 
 con_info_t ci = { nfalse, 0, 0, 0, NULL };
 
-/* Please see the file INTERNALS to help you understand why I do what
- * I do with BUFFER.
- * This function finds the next available space in BUFFER
- */
-static char *
-_nss_mysql_find_eol (char *buffer, int buflen)
-{
-  char *p = buffer + buflen;
-  int cur = buflen;
-
-  function_enter;
-  while (cur >= 1)
-    {
-      if (*p == 0 && *(p - 1) != 0)
-        {
-          function_leave;
-          return (p + 1);
-        }
-      p--;
-      cur--;
-    }
-  if (cur == 0 && *p == 0)
-    {
-      function_leave;
-      return (p);
-    }
-
-  _nss_mysql_debug (FNAME, D_PARSE, "eol not found");
-  function_leave;
-  return (NULL);
-}
-
 /* Number of comma- or space-separated tokens in BUFFER */
 static NSS_STATUS
 _nss_mysql_count_tokens (const char *buffer, int *count)
@@ -80,38 +48,28 @@ _nss_mysql_count_tokens (const char *buffer, int *count)
     function_return (NSS_SUCCESS);
 
   /* strtok destroys the input, so copy it somewhere safe first */
-  _nss_mysql_debug (FNAME, D_MEMORY, "p = malloc (%d)", buflen);
-  if ((p = (char *) malloc (buflen + 1)) == NULL)
-    {
-      _nss_mysql_log (LOG_ALERT, "malloc of %d bytes failed", buflen + 1);
-      function_return (NSS_UNAVAIL);
-    }
+  if ((p = (char *) xmalloc (buflen + 1)) == NULL)
+    function_return (NSS_UNAVAIL);
   memcpy (p, buffer, strlen (buffer) + 1);
 
-  *count = 0;
-  token = strtok (p, ", ");
-  while (token)
-    {
-      (*count)++;
-      token = strtok (NULL, ", ");
-    }
-  _nss_mysql_debug (FNAME, D_MEMORY, "free (p)");
-  free (p);
+  for (*count = 0, token = strtok (p, ", "); token; (*count)++)
+    token = strtok (NULL, ", ");
+  xfree (p);
   _nss_mysql_debug (FNAME, D_PARSE, "Found %d tokens", *count);
   function_return (NSS_SUCCESS);
 }
 
-/* 'lis' = Load Into Structure.  'wb' = With Buffer
+/*
+ * 'lis' = Load Into Structure.  'wb' = With Buffer
  * There's a different version of this (just 'lis' and not 'liswb')
  * in nss_config.c
  */
 static NSS_STATUS
 _nss_mysql_liswb (const char *val, void *structure, char *buffer,
-                  size_t buflen, int fofs, ftype_t type)
+                  size_t buflen, int *bufused, int fofs, ftype_t type)
 {
   _nss_mysql_byte *b;
-  int val_length = 0;
-  char *sos;    /* Start of String(s) */
+  int val_size = 0;
 
   function_enter;
   if (!val)
@@ -119,7 +77,7 @@ _nss_mysql_liswb (const char *val, void *structure, char *buffer,
       _nss_mysql_log (LOG_CRIT, "%s was passed a NULL VAL");
       function_return (NSS_UNAVAIL);
     }
-  val_length = strlen (val);
+  val_size = strlen (val) + 1;
 
   b = (_nss_mysql_byte *) structure;
   switch (type)
@@ -137,33 +95,23 @@ _nss_mysql_liswb (const char *val, void *structure, char *buffer,
       *(unsigned long *) (b + fofs) = (unsigned long) atol (val);
       break;
     case FT_PCHAR:
-      sos = _nss_mysql_find_eol (buffer, buflen);
-      if (sos == NULL)
+      if (*bufused > buflen)
         {
           errno = ERANGE;
           _nss_mysql_debug (FNAME, D_MEMORY,
                             "Requesting more memory for BUFFER");
           function_return (NSS_TRYAGAIN);
         }
-
-      if (buflen - (intptr_t) (sos - buffer) <= val_length)
-        {
-          errno = ERANGE;
-          _nss_mysql_debug (FNAME, D_MEMORY,
-                            "Requesting more memory for BUFFER");
-          function_return (NSS_TRYAGAIN);
-        }
-
-      *(intptr_t *) (b + fofs) = (intptr_t) memcpy (sos, val, val_length);
+      *(intptr_t *) (b + fofs) = (intptr_t) memcpy (buffer, val, val_size);
+      *bufused += val_size;
       break;
     case FT_PPCHAR:
       {
-        char *sop;           /* Start of Pointers */
+        char *s;    /* Strings start here */
+        char *p;    /* Pointers start here */
         char *copy_of_val;
         char *token;
-        int num_tokens, token_length;
-        int token_num;       /* Current token number */
-        int offset;          /* # bytes ahead of sos to place this string */
+        int num_tokens, token_size;
 
         if ((_nss_mysql_count_tokens (val, &num_tokens)) != NSS_SUCCESS)
           function_return (NSS_UNAVAIL);
@@ -171,55 +119,39 @@ _nss_mysql_liswb (const char *val, void *structure, char *buffer,
         if (num_tokens == 0)
           function_return (NSS_SUCCESS);
 
-        /* Pointer storage will start here */
-        if ((sop = _nss_mysql_find_eol (buffer, buflen)) == NULL)
-          {
-            errno = ERANGE;
-            _nss_mysql_debug (FNAME, D_MEMORY,
-                              "Requesting more memory for BUFFER");
-            function_return (NSS_TRYAGAIN);
-          }
-
         /* Leave room for n+1 pointers (last must be NULL) */
-        sos = sop + ((num_tokens + 1) * PTRSIZE);
+        p = buffer;
+        s = p + ((num_tokens + 1) * PTRSIZE);
+        *bufused += (num_tokens + 1) * PTRSIZE;
 
         /* strtok destroys the input, so copy it somewhere safe first */
-        _nss_mysql_debug (FNAME, D_MEMORY, "copy_of_val = malloc (%d)",
-                          val_length + 1);
-        if ((copy_of_val = (char *) malloc (val_length + 1)) == NULL)
-          {
-            _nss_mysql_log (LOG_ALERT, "malloc of %d bytes failed",
-                            val_length + 1);
-            function_return (NSS_UNAVAIL);
-          }
+        if ((copy_of_val = (char *) xmalloc (val_size)) == NULL)
+          function_return (NSS_UNAVAIL);
 
-        memcpy (copy_of_val, val, val_length + 1);
+        memcpy (copy_of_val, val, val_size);
 
-        token_num = offset = 0;
         token = strtok (copy_of_val, ", ");
         while (token)
           {
-            token_length = strlen (token);
-            if (buflen - (intptr_t) (sos - buffer) <= token_length + 1)
+            token_size = strlen (token) + 1;
+            *bufused += token_size;
+            if (*bufused > buflen)
               {
-                _nss_mysql_debug (FNAME, D_MEMORY, "free (copy_of_val)");
-                free (copy_of_val);
+                xfree (copy_of_val);
                 errno = ERANGE;
                 _nss_mysql_debug (FNAME, D_MEMORY,
                                   "Requesting more memory for BUFFER");
                 function_return (NSS_TRYAGAIN);
               }
-            *(intptr_t *) (sop + (token_num * PTRSIZE)) =
-              (intptr_t) memcpy (sos + offset, token, token_length + 1);
-            token_num++;
-            offset += token_length + 1;
+            *(intptr_t *) p = (intptr_t) memcpy (s, token, token_size);
+            p += PTRSIZE;
+            s += token_size;
 
             token = strtok (NULL, ", ");
           }
-        _nss_mysql_debug (FNAME, D_MEMORY, "free (copy_of_val)");
-        free (copy_of_val);
+        xfree (copy_of_val);
         /* Set structure pointer to point at start of pointers */
-        *(intptr_t *) (b + fofs) = (intptr_t) sop;
+        *(intptr_t *) (b + fofs) = (intptr_t) buffer;
       }
       break;
     default:
@@ -232,17 +164,18 @@ _nss_mysql_liswb (const char *val, void *structure, char *buffer,
 static freturn_t
 _nss_mysql_save_socket_info (void)
 {
-  socklen_t sockaddr_size = sizeof (struct sockaddr);
+  socklen_t local_size = sizeof (struct sockaddr);
+  socklen_t remote_size = sizeof (struct sockaddr);
   int r;
 
   function_enter;
-  r = getsockname (ci.link.net.fd, &(ci.sock_info.local),
-                   &sockaddr_size) ;
+  memset(&(ci.sock_info.local), 0, sizeof (ci.sock_info.local));
+  r = getsockname (ci.link.net.fd, &(ci.sock_info.local), &local_size) ;
   if (r != RETURN_SUCCESS)
     function_return (RETURN_FAILURE);
 
-  r = getpeername (ci.link.net.fd, &(ci.sock_info.remote),
-                   &sockaddr_size) ;
+  memset(&(ci.sock_info.remote), 0, sizeof (ci.sock_info.remote));
+  r = getpeername (ci.link.net.fd, &(ci.sock_info.remote), &remote_size);
   function_return (r);
 }
 
@@ -250,7 +183,6 @@ static nboolean
 _nss_mysql_is_same_sockaddr (struct sockaddr orig, struct sockaddr cur)
 {
   struct sockaddr_in orig_sin, cur_sin;
-  struct sockaddr_un orig_sun, cur_sun;
   sa_family_t family;
 
   function_enter;
@@ -266,13 +198,11 @@ _nss_mysql_is_same_sockaddr (struct sockaddr orig, struct sockaddr cur)
           function_return (nfalse);
         break;
     case AF_UNIX:
-        orig_sun = *(struct sockaddr_un *) &orig;
-        cur_sun = *(struct sockaddr_un *) &cur;
-        if (strcmp (orig_sun.sun_path, cur_sun.sun_path) != 0)
+        if (memcmp (&orig, &cur, sizeof (struct sockaddr)) != 0)
           function_return (nfalse);
         break;
     default:
-        _nss_mysql_log (LOG_ERR, "%s: Unhandled family: %d", family, FNAME);
+        _nss_mysql_log (LOG_ERR, "%s: Unhandled family: %d", FNAME, family);
         break;
     }
   function_return (ntrue);
@@ -281,33 +211,32 @@ _nss_mysql_is_same_sockaddr (struct sockaddr orig, struct sockaddr cur)
 static nboolean
 _nss_mysql_validate_socket (void)
 {
-  socklen_t sockaddr_size = sizeof (struct sockaddr);
+  socklen_t local_size = sizeof (struct sockaddr);
+  socklen_t remote_size = sizeof (struct sockaddr);
   struct sockaddr check;
-  int r;
 
   function_enter;
-  r = getsockname (ci.link.net.fd, &check, &sockaddr_size);
-  if (r != ntrue)
-    {
-      _nss_mysql_debug (FNAME, D_CONNECT, "getsockname() failed");
-      function_return (nfalse);
-    }
-  r = _nss_mysql_is_same_sockaddr (ci.sock_info.local, check);
-  if (r != ntrue)
-    {
-      _nss_mysql_debug (FNAME, D_CONNECT, "local socket mismatch");
-      function_return (nfalse);
-    }
-  r = getpeername (ci.link.net.fd, &check, &sockaddr_size);
-  if (r != ntrue)
+  memset(&check, 0, sizeof (check));
+  if (getpeername (ci.link.net.fd, &check, &remote_size) != RETURN_SUCCESS)
     {
       _nss_mysql_debug (FNAME, D_CONNECT, "getpeername() failed");
       function_return (nfalse);
     }
-  r = _nss_mysql_is_same_sockaddr (ci.sock_info.remote, check);
-  if (r != ntrue)
+  if (_nss_mysql_is_same_sockaddr (ci.sock_info.remote, check) != ntrue)
     {
       _nss_mysql_debug (FNAME, D_CONNECT, "remote socket mismatch");
+      function_return (nfalse);
+    }
+
+  memset(&check, 0, sizeof (check));
+  if (getsockname (ci.link.net.fd, &check, &local_size) != RETURN_SUCCESS)
+    {
+      _nss_mysql_debug (FNAME, D_CONNECT, "getsockname() failed");
+      function_return (nfalse);
+    }
+  if (_nss_mysql_is_same_sockaddr (ci.sock_info.local, check) != ntrue)
+    {
+      _nss_mysql_debug (FNAME, D_CONNECT, "local socket mismatch");
       function_return (nfalse);
     }
 
@@ -522,6 +451,8 @@ _nss_mysql_load_result (void *result, char *buffer, size_t buflen,
   field_info_t *f;
   int i;
   int num_rows;
+  char *p;
+  int bufused;
 
   function_enter;
   num_rows = mysql_num_rows (ci.result);
@@ -543,11 +474,13 @@ _nss_mysql_load_result (void *result, char *buffer, size_t buflen,
     }
 
   memset (buffer, 0, buflen);
+  bufused = 0;
   for (f = fields, i = 0; f->name; f++, i++)
     {
+      p = buffer + bufused;
       _nss_mysql_debug (FNAME, D_PARSE, "Loading: %s = '%s' (%d %d %d)",
                         f->name, row[i], i, f->ofs, f->type);
-      to_return = _nss_mysql_liswb (row[i], result, buffer, buflen,
+      to_return = _nss_mysql_liswb (row[i], result, p, buflen, &bufused,
                                     f->ofs, f->type);
       if (to_return != NSS_SUCCESS)
         function_return (to_return);
