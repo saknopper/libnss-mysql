2f24e5e3e67794ce52a792210a7c710b159807a5
Ben Goodwin <cinergi@users.sourceforge.net>
2002-11-30 19:41:38 +0000

    pass q, not &q to build_query Don't build query if we're not gonna run it! More specific debugging

--- lookup.c
+++ lookup.c
@@ -26,7 +26,7 @@ extern conf_t conf;
 
 static NSS_STATUS
 _nss_mysql_build_query (lookup_t ltype, const char *name, unsigned int num,
-                        char **qin, char **qout, MYSQL_RES **mresult,
+                        char *qin, char **qout, MYSQL_RES **mresult,
                         const char *caller)
 {
   DN ("_nss_mysql_build_query")
@@ -36,16 +36,16 @@ _nss_mysql_build_query (lookup_t ltype, const char *name, unsigned int num,
   DENTER
   *qout = NULL;
 
-  if (!*qin || strlen (*qin) == 0)
+  if (!qin || strlen (qin) == 0)
     {
      _nss_mysql_log (LOG_CRIT, "%s has no valid query in config", caller);
-     DIRETURN (NSS_UNAVAIL)
+     DSRETURN (NSS_UNAVAIL)
     }
 
-  qout_size = strlen (*qin) + PADSIZE + 1;
+  qout_size = strlen (qin) + PADSIZE + 1;
   *qout = _nss_mysql_malloc (qout_size);
   if (*qout == NULL)
-    DIRETURN (NSS_UNAVAIL)
+    DSRETURN (NSS_UNAVAIL)
 
   switch (ltype)
     {
@@ -53,37 +53,37 @@ _nss_mysql_build_query (lookup_t ltype, const char *name, unsigned int num,
       if (strlen (name) == 0)
         {
           _nss_mysql_free (*qout);
-          DIRETURN (NSS_NOTFOUND)
+          DSRETURN (NSS_NOTFOUND)
         }
       clean_name = _nss_mysql_malloc (strlen (name) * 2 + 1);
       if (clean_name == NULL)
         {
           _nss_mysql_free (*qout);
-          DIRETURN (NSS_UNAVAIL)
+          DSRETURN (NSS_UNAVAIL)
         }
       if (_nss_mysql_escape_string (clean_name, name, mresult) != NSS_SUCCESS)
         {
           _nss_mysql_free (*qout);
           _nss_mysql_free (clean_name);
-          DIRETURN (NSS_UNAVAIL)
+          DSRETURN (NSS_UNAVAIL)
         }
-      snprintf (*qout, qout_size, *qin, clean_name);
+      snprintf (*qout, qout_size, qin, clean_name);
       _nss_mysql_free (clean_name);
       _nss_mysql_reset_ent (mresult);
       break;
     case BYNUM:
-      snprintf (*qout, qout_size, *qin, num);
+      snprintf (*qout, qout_size, qin, num);
       _nss_mysql_reset_ent (mresult);
       break;
     case BYNONE:
       if (!mresult || !*mresult)
-        strcpy (*qout, *qin);
+        strcpy (*qout, qin);
       break;
     default:
       _nss_mysql_free (*qout);
-      DIRETURN (NSS_UNAVAIL)
+      DSRETURN (NSS_UNAVAIL)
     }
-  DIRETURN (NSS_SUCCESS)
+  DSRETURN (NSS_SUCCESS)
 }
 
 NSS_STATUS
@@ -97,34 +97,29 @@ _nss_mysql_lookup (lookup_t ltype, const char *name, unsigned int num,
   DN ("_nss_mysql_lookup")
   char *query;
   int retVal;
-  nboolean run_query = ntrue;
 
   DENTER
   if (restricted == ntrue && geteuid () != 0)
-    DIRETURN (NSS_NOTFOUND)
+    DSRETURN (NSS_NOTFOUND)
 
   if (_nss_mysql_init () != NSS_SUCCESS)
-    DIRETURN (NSS_UNAVAIL)
+    DSRETURN (NSS_UNAVAIL)
 
-  /* Create query string using config & args; QUERY is malloc'ed! */
-  retVal = _nss_mysql_build_query (ltype, name, num, q, &query, mresult,
-                                   caller);
-  if (retVal != NSS_SUCCESS)
-    DIRETURN (retVal)
-
-  /* BYNONE indicates *ent; don't run the query if we already did */
-  if (ltype == BYNONE && *mresult)
-    run_query = nfalse;
-
-  /* Send just-build query string to MySQL server */
-  if (run_query)
-    retVal = _nss_mysql_run_query (query, mresult);
-  else
-    retVal = NSS_SUCCESS;
-
-  _nss_mysql_free (query);
-  if (retVal != NSS_SUCCESS)
-    DIRETURN (retVal)
+  /* BYNONE indicates *ent; don't create/run the query since we already did */
+  if (!(ltype == BYNONE && *mresult))
+    {
+      /* Create query string using config & args; QUERY is malloc'ed! */
+      retVal = _nss_mysql_build_query (ltype, name, num, *q, &query, mresult,
+                                       caller);
+      if (retVal != NSS_SUCCESS)
+        DSRETURN (retVal)
+      retVal = _nss_mysql_run_query (query, mresult);
+      if (retVal != NSS_SUCCESS)
+        {
+          _nss_mysql_free (query);
+          DSRETURN (retVal)
+        }
+    }
 
   /* Take result of query and load RESULT & BUFFER */
   retVal = load_func (result, buffer, buflen, *mresult, errnop);
@@ -133,6 +128,6 @@ _nss_mysql_lookup (lookup_t ltype, const char *name, unsigned int num,
   if (ltype != BYNONE)
     _nss_mysql_close_result (mresult);
 
-  DIRETURN (retVal)
+  DSRETURN (retVal)
 }
 
