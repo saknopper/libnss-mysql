13a21fac0e7f180b0a66f24fba49b6a428d61859
Ben Goodwin <cinergi@users.sourceforge.net>
2004-04-11 23:18:36 +0000

    Make aux dir if it doesn't exist

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 # Quick script to set up a build environment from CVS
 ############################################################################
 set -x
+test ! -d aux && mkdir aux
 aclocal -I aux && \
 autoheader && \
 autoconf && \
